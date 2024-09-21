2a1e7b229dfcabfceafe41f9b05c18bef188a51e
Kanompang <suparadajk21@gmail.com>
2024-09-21 20:53:49 +0700

    BP5-Kanompang

--- question-8.js
+++ question-8.js
@@ -1,6 +1,19 @@
 // Question #8: Fetching User List from Server
 
 // เริ่มเขียนโค้ดตรงนี้
+let getUsers = async () => {
+    const response = await fetch ("https://jsonplaceholder.typicode.com/users");
+    const data = await new Promise(resolve => setTimeout(() => resolve(response.json())));
+    return data;
+};
+
+async function getNewUsers() {
+    const Users = await getUsers();
+    const newUsers = Users.map(function (user){return user.name}); 
+    console.log(newUsers);
+  }
+   
+getNewUsers();
 
 /* map +function async
 - ให้ Fetch ข้อมูล User จาก Server ของ Jsonplaceholder ด้วย Built-in `fetch` Function
